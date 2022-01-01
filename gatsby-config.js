e7603d75505def0ba57a1d465f22d1e92b368074
Mike Babetchki <mike.babetchki@gmail.com>
2022-01-01 14:50:07 -0600

    Setup initail deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,40 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Enira Beauty Room",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    `gatsby-theme-material-ui`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `gallery`,
+        path: `${__dirname}/src/gallery`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+  ],
 };
